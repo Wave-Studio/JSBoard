650fd0aadaa4cda8fc4b470516e9a97e67e8cdef
Blocksnmore <Blocksnmore@users.noreply.github.com>
2021-05-08 20:59:41 +0000

    Prettified Code!

--- components/notification.js
+++ components/notification.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import Link from "next/link";
 
-export default function Notification(props) { 
-    return (
-      <>
-        <div
-          className={
-            "flex rounded px-4 py-2 z-30 absolute bottom-[20px] right-[30px] text-center text-white font-semibold shadow-xl select-none " +
-            props.color
-          }
-        >
-          {props.msg}
-        </div>
-      </>
-    );
-  }
+export default function Notification(props) {
+  return (
+    <>
+      <div
+        className={
+          "flex rounded px-4 py-2 z-30 absolute bottom-[20px] right-[30px] text-center text-white font-semibold shadow-xl select-none " +
+          props.color
+        }
+      >
+        {props.msg}
+      </div>
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../styles/global.css";
 
-
 function JSboard({ Component, pageProps }) {
   return (
     <div className="flex flex-col h-screen bg-coolGray-700">
