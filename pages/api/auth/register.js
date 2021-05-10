442ea240ec5ac349ccbdeb12bc155b8107359343
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-05-10 13:35:51 -0500

    format

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -4,5 +4,9 @@ export default async function (req, res) {
   if (!fs.existsSync("./.jsboard/config.json"))
     return res
       .status(500)
-      .json({ error: 500, description: "JSBoard is not configured yet!", configured: false, });
+      .json({
+        error: 500,
+        description: "JSBoard is not configured yet!",
+        configured: false,
+      });
 }
--- pages/api/config/createconfig.js
+++ pages/api/config/createconfig.js
@@ -10,5 +10,9 @@ export default function (req, res) {
   if (checkConfig())
     return res
       .status(404)
-      .json({ error: 404, description: "JSBoard is already configured!", configured: true, });
+      .json({
+        error: 404,
+        description: "JSBoard is already configured!",
+        configured: true,
+      });
 }
