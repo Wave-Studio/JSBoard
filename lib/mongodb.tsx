9fc911fc4465006f29a841dbb3feaaac07624c2a
Lukas <69743171+quick007@users.noreply.github.com>
2021-09-23 21:24:54 -0700

    see future schedules

--- lib/mongodb.tsx
+++ lib/mongodb.tsx
@@ -1,22 +1,32 @@
 import mongoose from 'mongoose';
 
-const connectDB = handler => async (req: any, res: any) => {
-  if (mongoose.connections[0].readyState) {
-    // Use current db connection
-    return handler(req, res);
-  }
-  const options = {
-    poolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useNewUrlParser: true
-  };
-  // Use new db connection
-  await mongoose.connect(process.env.DB_LINK, options);
-  return handler(req, res);
+const uri = process.env.DB_LINK
+const options = {
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 };
 
-export default connectDB;
+
+let clientPromise: Promise<typeof mongoose>;
+
+if (!process.env.DB_LINK) {
+  throw new Error('Please add your Mongo URI to .env.local')
+}
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!mongoose.connections[0].readyState) {
+    clientPromise = mongoose.connect(process.env.DB_LINK, options)
+  }
+  clientPromise = global._mongoClientPromise
+} else if (!mongoose.connections[0].readyState) {
+  // In production mode, it's best to not use a global variable.
+  clientPromise = mongoose.connect(process.env.DB_LINK, options)
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise
--- pages/api/forums/homepage.tsx
+++ pages/api/forums/homepage.tsx
@@ -1,35 +1,62 @@
 import checkConfig from "../../../lib/checkConfig";
 import { Database } from "quickmongo";
+import clientPromise from '../../../lib/mongodb';
 import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
+let Homepage: mongoose.Model<mongoose.Document<any>>;
 
-import connectDB from "../../../lib/mongodb"
 //const db = new Database(process.env.DB_LINK);
 
-//handle().catch(err => console.log(err));
-
-async function handle(req, res) {
-  
-  const homepageSchema = new Schema({
-    configured: Boolean,
-      forums: {
-        type: Map,
-        of: Object
-      },
-      description: String,
-      // svgs and json are kinda 🤡, so I had to do what I did below to make this work
-      store: Boolean, //true = enabled, false = not enabled
-      storeLink: String, //uses camelCase cause why not
-      website: Boolean,
-      websiteLink: String,
-      custom: Boolean,
-      customName: String, //custom link also gets a name
-      customLink: String, 
-      orgName: String
-  })
-  const Homepage = mongoose.model('Homepage', homepageSchema);
-  const orgName = new Homepage({ orgName: 'Azynnet' });
+export default async function handle(req, res) {
+  await clientPromise;
+  await mongoose.connections[0].readyState;
+  /*if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handle(req, res);
+  }
+  const options = {
+    poolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true
+  };
+  // Use new db connection
+  await mongoose.connect(process.env.DB_LINK, options);
+  return handle(req, res);*/
+  if (!mongoose.models.Homepage) {
+    var homepageSchema = new Schema({
+      configured: Boolean,
+        forums: [{
+          icon: String,
+          name: String,
+          description: String,
+          redirect: String,
+          color: String, //won't work, purgecss needs to see full names
+          topics: Number,
+          posts: Number,
+        }],
+        description: String,
+        // svgs and json are kinda 🤡, so I had to do what I did below to make this work
+        store: Boolean, //true = enabled, false = not enabled
+        storeLink: String,
+        website: Boolean,
+        websiteLink: String,
+        custom: Boolean,
+        customName: String, //custom link also gets a name
+        customLink: String, 
+        orgName: String
+    })
+    
+  }
+  Homepage = mongoose.model('Homepage', homepageSchema)
+  const orgName = new Homepage({ configured: false });
   await orgName.save()
+  
+  return res.status(200).json(await mongoose.get('Homepage'))
+
 
   /*
   // TODO: make the site re-connect to the db every time to cause less issues n stuff
@@ -88,4 +115,3 @@ async function handle(req, res) {
 
 }
 
-export default connectDB(handle)
